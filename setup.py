440010d76a73d15cc049f337d48fa6a003aa7fb4
Luis Zarrabeitia <zarrabeitia@gmail.com>
2014-06-30 11:57:36 -0400

    Adding package info to setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,21 @@ module1 = Extension('csparsemunkres',
                     sources = ['pythonmodule.cpp', 'matrix.cpp', 'munkres.cpp'])
 
 setup (name = 'csparsemunkres',
-       version = '1.0',
-       description = 'This is a demo package',
-       ext_modules = [module1])
+        version = '1.0',
+        description = 'kuhn-munkres algorithm for the Assignment Problem with an incomplete cost matrix',
+        license = "GPLv2",
+        author = "Luis A. Zarrabeitia",
+        author_email = "zarrabeitia at gmail dot com",
+        ext_modules = [module1],
+        classifiers = [
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: GPLv2',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Scientific/Engineering :: Mathematics',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+)
 
 
